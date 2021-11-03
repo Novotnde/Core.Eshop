e02c85513b6091d289477910ea004d9c868bf078
novotnde <novot@DESKTOP-G2UJMF1>
2021-11-03 22:12:06 +0100

    usage of productservice

---
+++ src/Core.Api/Core.Api/Controllers/ProductController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Contracts;
+using Core.Contracts.Contracts;
+using Core.Contracts.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Core.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : Controller
+    {
+
+        private readonly ILogger<ProductController> _logger;
+        private readonly IProductService _productService;
+
+        public ProductController(ILogger<ProductController> logger, IProductService productService)
+        {
+            _logger = logger;
+            _productService = productService;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetProductById(int id)
+        {
+            var products = _productService.GetProductByIdAsync(id);
+            return Ok(products);
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var products = _productService.GetProductsAsync();
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct([FromBody] Product product)
+        {
+            var result = _productService.CreateProductAsync(product);
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateProductDescription(int id, string description)
+        {
+            var result = _productService.UpdateProductDescriptionAsync(id, description);
+            return Ok(result);
+        }
+    }
+}
--- src/Core.Api/Core.Api/Startup.cs
+++ src/Core.Api/Core.Api/Startup.cs
@@ -1,16 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Core.Contracts;
+using Microsoft.EntityFrameworkCore;
+using Core.Data;
 
 namespace Core.Api
 {
@@ -26,12 +23,28 @@ namespace Core.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            var connection = Configuration.GetConnectionString("DbContext");
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connection), ServiceLifetime.Transient);
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Core.Api", Version = "v1" });
             });
 
             services.AddControllers();
+
+            services.AddServicesForCoreLogic();
+
+            services.AddApiVersioning(setup =>
+            {
+                setup.DefaultApiVersion = new ApiVersion(1, 0);
+                setup.AssumeDefaultVersionWhenUnspecified = true;
+                setup.ReportApiVersions = true;
+                setup.ApiVersionReader = new QueryStringApiVersionReader("version");
+            });
+
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +61,8 @@ namespace Core.Api
             {
                 endpoints.MapControllers();
             });
+
+            app.UseApiVersioning();
         }
     }
 }
