5344bc52759f7329f91d8a8c0726672714064636
novotnde <novot@DESKTOP-G2UJMF1>
2021-11-03 22:11:38 +0100

    added database context

---
+++ src/Core.Api/Core.Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Core.Contracts.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Product { get; set; }
+    }
+}
